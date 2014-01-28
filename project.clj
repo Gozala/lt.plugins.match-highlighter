3479137a005fbb93f63ad4951c1c6d248d26d26b
Irakli Gozalishvili <rfobic@gmail.com>
2014-01-28 10:55:59 -0800

    Version 0.0.3

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.jeditoolkit/match-highlighter "0.0.2"
+(defproject com.jeditoolkit/match-highlighter "0.0.3"
   :description "Light Table plugin for highlighting matcking tokens"
   :dependencies [[org.clojure/clojure "1.5.1"]])
